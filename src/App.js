961d81e74a602b56ae6754ce741f179736b71fc6
PriankaCodes <85863824+PriankaCodes@users.noreply.github.com>
2021-12-04 18:03:50 +1100

    Initial commit - Project set up

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import pinboardBackground from "./pinboardBackground.jpg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={pinboardBackground} alt="Pinboard background"></img>
+      </header>
+    </div>
+  );
+}
+
+export default App;

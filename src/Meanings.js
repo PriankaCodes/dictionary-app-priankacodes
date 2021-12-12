faa204ececcacdf6967c6371a0947999c69642b9
PriankaCodes <85863824+PriankaCodes@users.noreply.github.com>
2021-12-12 12:05:14 +1100

    Created Synonym component to display synonyms for each meaning

--- src/Meanings.js
+++ src/Meanings.js
@@ -1,16 +1,21 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meanings(props) {
   return (
     <div>
       <h3>{props.meaning.partOfSpeech}</h3>
       {props.meaning.definitions.map(function (definitions, index) {
+        console.log(definitions);
         return (
           <div key={index}>
             <p>
+              <strong>Definition: </strong>
               {definitions.definition}
               <br />
+              <strong>Example: </strong>
               <em>{definitions.example}</em>
+              <Synonyms synonyms={definitions.synonyms} />
             </p>
           </div>
         );
